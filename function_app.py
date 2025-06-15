56db61e6c3ec4708ee7f73a02d42828ba68597f1
prashantkumarll <prashantkumarll@gmail.com>
2025-06-15 23:18:49 +0530

    updated code

--- function_app.py
+++ function_app.py
@@ -3,8 +3,10 @@ import logging
 
 app = func.FunctionApp()
 
+
+@app.function_name("queue_function")
 @app.queue_trigger(arg_name="azqueue", queue_name="test",
-                               connection="19af6a_STORAGE") 
+                               connection="QueueStorage") 
 def queue_trigger(azqueue: func.QueueMessage):
     logging.info('Python Queue trigger processed a message: %s',
                 azqueue.get_body().decode('utf-8'))
